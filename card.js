928373677389ed528b4988440a17bc885ca63e15
Vearstingen <Ante_543@hotmail.com>
2025-04-08 01:00:50 +0200

    Delete card.js

--- card.js
+++
@@ -1,2 +0,0 @@
-export const Card = ({ children }) => <div className="border rounded shadow">{children}</div>;
-export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
